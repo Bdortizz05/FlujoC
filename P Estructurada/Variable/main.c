da4a8996072659cebb755c7880e0915efdb4e93c
Brian Ortiz <bdortiz05@ucatolica.edu.co>
2022-04-05 17:23:01 -0500

    Agregar nuevo curso  de programacion estructurada

---
+++ P Estructurada/Variable/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int main()
+{
+
+    int PoL = 1 ;
+    float floatn = 3.1416 ;
+    char letterA = 'A' ;
+    bool boolean = false ;
+
+
+
+
+
+
+
+
+    return 0;
+}
